1f808d52bd10d8226964e34fd08c097308338d99
yato03 <miguelhs3523@gmail.com>
2024-05-10 16:46:01 +0200

    concurrency

--- app/fuzzer.go
+++ app/fuzzer.go
@@ -7,11 +7,11 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"log"
 )
 
 func main() {
 
+	// Arguments
 	var (
 		flURL      = flag.String("url", "", "URL to fuzz")
 		flWordlist = flag.String("wordlist", "", "Wordlist to use")
@@ -41,26 +41,53 @@ func main() {
 	wordlistPath := strings.TrimSpace(*flWordlist)
 	wordlist := readWordlist(wordlistPath)
 
-	fuzzUrl(url, wordlist)
+	// Create channels
+	words := make(chan string, 100)
+	results := make(chan string)
+	var urls []string
+
+	for i := 0; i < cap(words); i++ {
+		go fuzzUrl(url, words, results)
+	}
+
+	go func() {
+		for _, word := range wordlist {
+			words <- word
+		}
+	}()
+
+	for _, word := range wordlist {
+		url := <-results
+		if word != "" {
+			urls = append(urls, url)
+		}
+	}
+
+	close(words)
+	close(results)
 }
 
-func fuzzUrl(url string, wordlist []string) {
-	for _, word := range wordlist {
+func fuzzUrl(url string, words, results chan string) {
+	for word := range words {
 		newUrl := url + word
-		go checkUrl(newUrl)
+		exists := checkUrl(newUrl)
+		if exists {
+			fmt.Println("URL exists: ", newUrl)
+			results <- newUrl
+		} else{
+			results <- ""
+		}
 	}
 }
 
-func checkUrl(url string) {
+func checkUrl(url string) bool {
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return false
 	}
 	defer res.Body.Close()
 
-	if res.StatusCode == http.StatusOK {
-		fmt.Println("URL exists: ", url)
-	}
+	return res.StatusCode == http.StatusOK
 }
 
 func readWordlist(wordlistPath string) []string {
